6f25040adb5a33003a1d17e7e370613dcf32d612
joseppi <i.piperakis@gmail.com>
2021-10-09 00:47:09 +0300

    Add a basic Body component

---
+++ src/components/Body.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Container} from 'react-bootstrap'
+
+class Body extends React.Component {
+    render() { 
+        return (
+        <Container>
+            <h1>Hello Phylax!</h1>
+        </Container>
+        )
+    }
+}
+ 
+export default Body;
