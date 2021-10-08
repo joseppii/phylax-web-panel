fe2e85fee6020ac3cf6f4bdd311035010452373b
joseppi <i.piperakis@gmail.com>
2021-10-09 00:46:37 +0300

    Add basic Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Container} from 'react-bootstrap'
+
+class Footer extends React.Component {
+    render() { 
+        return (
+            <Container>
+                <p>Footer</p>
+            </Container>
+        )
+    }
+}
+ 
+export default Footer;
